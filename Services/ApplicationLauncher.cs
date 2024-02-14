d3e8f6e2c4e808ec58dc9be2eb4754cdb1bc27c1
VLTNO <60850875+VLTNOgithub@users.noreply.github.com>
2024-02-14 18:41:58 +0000

    Fixed window not focusing in ApplicationLauncher.cs

--- Services/ApplicationLauncher.cs
+++ Services/ApplicationLauncher.cs
@@ -1,4 +1,4 @@
-﻿using SuchByte.MacroDeck.Logging;
+using SuchByte.MacroDeck.Logging;
 using SuchByte.WindowsUtils.Utils;
 using System;
 using System.Diagnostics;
@@ -12,121 +12,147 @@ namespace SuchByte.WindowsUtils.Services;
 
 public class ApplicationLauncher
 {
-    [DllImport("user32.dll")]
-    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-    [DllImport("user32.dll")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool SetForegroundWindow(IntPtr hWnd);
-
-    [DllImport("user32.dll")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    static extern bool IsIconic(IntPtr hWnd);
-
-    [DllImport("kernel32.dll")]
-    public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
-
-    [DllImport("psapi.dll")]
-    static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In][MarshalAs(UnmanagedType.U4)] int nSize);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    static extern bool CloseHandle(IntPtr hObject);
 
 
-    private const int SW_MINIMIZE = 0;
-    private const int SW_RESTORE = 9;
+	[DllImport("user32.dll")]
+	public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+	[DllImport("user32.dll")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+	[DllImport("user32.dll")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	static extern bool IsIconic(IntPtr hWnd);
+
+	[DllImport("kernel32.dll")]
+	public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+	[DllImport("psapi.dll")]
+	static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In][MarshalAs(UnmanagedType.U4)] int nSize);
+
+	[DllImport("kernel32.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	static extern bool CloseHandle(IntPtr hObject);
 
 
-    public static void StartApplication(string path, string arguments, bool asAdmin)
-    {
-        var p = new Process
-        {
-            StartInfo = new ProcessStartInfo(path)
-            {
-                UseShellExecute = true,
-                WorkingDirectory = Path.GetDirectoryName(path),
-                Arguments = arguments,
-                Verb = asAdmin ? "runas" : ""
-            }
-        };
-        p.Start();
-    }
-
-    public static bool IsRunning(string path)
-    {
-        path = WindowsShortcut.GetShortcutTarget(path);
-        bool isRunning = GetProcessByPath(path) != null;
-        return isRunning;
-    }
-
-    public static void KillApplication(string path)
-    {
-        path = WindowsShortcut.GetShortcutTarget(path);
-        if (!IsRunning(path)) return;
-        var p = GetProcessByPath(path);
-        if (p == null) return;
-        Process.GetProcessesByName(p.ProcessName).ToList().ForEach(p =>
-        {
-            MacroDeckLogger.Trace(Main.Instance, $"Killing process: {p.ProcessName} PID: {p.Id}");
-            p.Kill();
-        });
-    }
-
-    public static void BringToForeground(string path)
-    {
-        path = WindowsShortcut.GetShortcutTarget(path);
-        if (!IsRunning(path)) return;
-        var p = GetProcessByPath(path);
-        if (p == null) return;
-
-        IntPtr handle = p.MainWindowHandle;
-        if (SetForegroundWindow(handle))
-        {
-            if (!IsIconic(handle))
-            {
-                return;
-            }
-        }
-        MinimizeAndRestoreWindow(handle); // Fallback function
-    }
-
-    public static Process GetProcessByPath(string path)
-    {
-        path = WindowsShortcut.GetShortcutTarget(path);
-        return Process.GetProcesses().ToArray().Where(p => GetProcessFileName(p.Id).Equals(path, StringComparison.OrdinalIgnoreCase)).OrderByDescending(p => p.Id).FirstOrDefault();
-    }
+	private const int SW_MINIMIZE = 0;
+	private const int SW_RESTORE = 9;
 
 
-    private static void MinimizeAndRestoreWindow(IntPtr hWnd)
-    {
-        ShowWindow(hWnd, SW_MINIMIZE);
-        ShowWindow(hWnd, SW_RESTORE);
-    }
+	public static void StartApplication(string path, string arguments, bool asAdmin)
+	{
+		var p = new Process
+		{
+			StartInfo = new ProcessStartInfo(path)
+			{
+				UseShellExecute = true,
+				WorkingDirectory = Path.GetDirectoryName(path),
+				Arguments = arguments,
+				Verb = asAdmin ? "runas" : ""
+			}
+		};
+		p.Start();
+	}
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static string GetProcessFileName(int pid)
-    {
-        var processHandle = OpenProcess(0x0400 | 0x0010, false, pid);
+	public static bool IsRunning(string path)
+	{
+		path = WindowsShortcut.GetShortcutTarget(path);
+		bool isRunning = GetProcessByPath(path) != null;
+		return isRunning;
+	}
 
-        if (processHandle == IntPtr.Zero)
-        {
-            return "";
-        }
+	public static void KillApplication(string path)
+	{
+		path = WindowsShortcut.GetShortcutTarget(path);
+		if (!IsRunning(path)) return;
+		var p = GetProcessByPath(path);
+		if (p == null) return;
+		Process.GetProcessesByName(p.ProcessName).ToList().ForEach(p =>
+		{
+			MacroDeckLogger.Trace(Main.Instance, $"Killing process: {p.ProcessName} PID: {p.Id}");
+			p.Kill();
+		});
+	}
 
-        const int lengthSb = 4000;
+	public static void BringToForeground(string path)
+	{
+		MacroDeckLogger.Info(Main.Instance, path);
+		string name = Path.GetFileName(path).Split(".")[0].ToString();
+        MacroDeckLogger.Info(Main.Instance, name);
 
-        var sb = new StringBuilder(lengthSb);
+        Process[] processes = Process.GetProcessesByName(name);
 
-        string result = null;
+		if (processes.Length > 0)
+		{
+			foreach (Process process in processes)
+			{
+				IntPtr hWnd = process.MainWindowHandle;
 
-        if (GetModuleFileNameEx(processHandle, IntPtr.Zero, sb, lengthSb) > 0)
-        {
-            result = sb.ToString();
-        }
+				if (hWnd != IntPtr.Zero)
+				{
+					ShowWindow(hWnd, 1);
+					SetForegroundWindow(hWnd);
+					return;
+				}
+			}
+		}
 
-        CloseHandle(processHandle);
+		/* OLD CODE */
 
-        return result;
-    }
+		//path = WindowsShortcut.GetShortcutTarget(path);
+		//Console.WriteLine(path);
+		//if (!IsRunning(path)) return;
+		//var p = GetProcessByPath(path);
+		//if (p == null) return;
+
+		//IntPtr handle = p.MainWindowHandle;
+		//if (SetForegroundWindow(handle))
+		//{
+		//    if (!IsIconic(handle))
+		//    {
+		//        return;
+		//    }
+		//}
+		//MinimizeAndRestoreWindow(handle); // Fallback function
+	}
+
+	public static Process GetProcessByPath(string path)
+	{
+		path = WindowsShortcut.GetShortcutTarget(path);
+		return Process.GetProcesses().ToArray().Where(p => GetProcessFileName(p.Id).Equals(path, StringComparison.OrdinalIgnoreCase)).OrderByDescending(p => p.Id).FirstOrDefault();
+	}
+
+
+	private static void MinimizeAndRestoreWindow(IntPtr hWnd)
+	{
+		ShowWindow(hWnd, SW_MINIMIZE);
+		ShowWindow(hWnd, SW_RESTORE);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static string GetProcessFileName(int pid)
+	{
+		var processHandle = OpenProcess(0x0400 | 0x0010, false, pid);
+
+		if (processHandle == IntPtr.Zero)
+		{
+			return "";
+		}
+
+		const int lengthSb = 4000;
+
+		var sb = new StringBuilder(lengthSb);
+
+		string result = null;
+
+		if (GetModuleFileNameEx(processHandle, IntPtr.Zero, sb, lengthSb) > 0)
+		{
+			result = sb.ToString();
+		}
+
+		CloseHandle(processHandle);
+
+		return result;
+	}
 }
